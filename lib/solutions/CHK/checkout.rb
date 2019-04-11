0fa13f53c822febd6c7f62e0637b51af8410526a
sbx_user1051 <sbx_user1051@169.254.105.105>
2019-04-11 14:13:35 +0000

    Thu Apr 11 14:13:35 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -2,10 +2,21 @@
 class Checkout
 
   def checkout(skus)
+    if bool return -1
+
 
   end
   private
 
-  
+  def not_proper(skus)
+    letters = skus.chars
+    bool = false
+    letters.each do |letter|
+      bool = true if !(["A","B","C","D"].include?(letter))
+    end
+    bool
+  end
+
 end
+
 
