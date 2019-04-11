f4496f2ac282d7fd76340337ebf939f910a7a736
sbx_user1051 <sbx_user1051@169.254.105.105>
2019-04-11 15:42:06 +0000

    Thu Apr 11 15:42:06 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -33,8 +33,8 @@ class Checkout
       price += time*20 if item == "C"
       price += time*15 if item == "D"
       price += (time*40) if item == "E"
-      price += ((time-(time/2))*10) if (item == "F" && item >=3 )
-      price += (time*10) if (item == "F" && item < 3 )
+      price += ((time-(time/2))*10) if (item == "F" && time >=3 )
+      price += (time*10) if (item == "F" && time < 3 )
     end
     price
   end
@@ -44,6 +44,7 @@ class Checkout
   end
 
 end
+
 
 
 
