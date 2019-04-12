a81a83b5043c0938eafca5b5af022ac176af677c
sbx_user1051 <sbx_user1051@169.254.39.181>
2019-04-12 15:27:11 +0000

    Fri Apr 12 15:27:11 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -117,9 +117,10 @@ class Checkout
     rep = hash["V"]
     # final = rep - rep/3*3- ((rep -rep/3*3)/2*2)
     # return (rep/3)*130 + (rep - rep/3*3)/2*90 + final*50
-    return (rep/3)*130 +(rep - (rep/3)*3)/2*90 
+    return (rep/3)*130
   end
 end
+
 
 
 
