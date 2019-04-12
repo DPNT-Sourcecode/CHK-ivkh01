b83f26bcb93bbb02901a707fc36d95bcc57641eb
sbx_user1051 <sbx_user1051@169.254.39.181>
2019-04-12 14:08:25 +0000

    Fri Apr 12 14:08:25 UTC 2019

---
+++ test/CHK/checkout_test.rb
@@ -0,0 +1,26 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'CHK'
+
+class ClientTest < Minitest::Test
+
+  def test_checkout
+    assert_equal -1 , Checkout.new.checkout("abc")
+    assert_equal 115, Checkout.new.checkout("ABCD")
+    assert_equal  255  ,Checkout.new.checkout("AAAABBB")
+    assert_equal 155 , Checkout.new.checkout("ABCDE")
+    assert_equal 80 , Checkout.new.checkout("EEB")
+    assert_equal 110, Checkout.new.checkout("EEBB")
+    assert_equal 200, Checkout.new.checkout("AAAAA")
+    assert_equal 250, Checkout.new.checkout("AAAAAA")
+    assert_equal 300, Checkout.new.checkout("AAAAAAA")
+    assert_equal 80, Checkout.new.checkout("EE")
+    assert_equal 20, Checkout.new.checkout("FF")
+    assert_equal 20, Checkout.new.checkout("FFF")
+    assert_equal 60, Checkout.new.checkout("FFFFFFFF")
+  end
+end
